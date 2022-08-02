957cb5fb2d747a5d6b5479a2f36ff972627e6131
Daniil Pogosyan <daniilpogosyan@gmail.com>
2022-08-02 14:00:01 +0500

    Create a sample of targets' position data

---
+++ src/data/targets.js
@@ -0,0 +1,30 @@
+const targets = [
+  {
+    name: 'shao-kahn',
+    origin: {
+      x: 0.7813186813186813,
+      y: 0.7008883685696577
+    },
+    radius: 0.04
+  },
+  {
+    name: 'sub-zero',
+    origin: {
+      x: 0.5560439560439561,
+      y: 0.5448660560567454
+    },
+    radius: 0.035
+  },
+  {
+    name: 'scorpion',
+    origin: {
+      x: 0.3758241758241758,
+      y: 0.5598512499139178
+    },
+    radius: 0.035
+  }
+]
+
+export function getTargets() {
+  return targets;
+}
