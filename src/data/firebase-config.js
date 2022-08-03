ecdb01a666555d9cef2699ae84417c3951a182d8
Daniil Pogosyan <daniilpogosyan@gmail.com>
2022-08-03 13:34:03 +0500

    Install firebase, set config

---
+++ src/data/firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAXo1HNjTDOX-1s79yf8BRqZ16z6vxSbjs",
+  authDomain: "where-is-waldo-a04da.firebaseapp.com",
+  projectId: "where-is-waldo-a04da",
+  storageBucket: "where-is-waldo-a04da.appspot.com",
+  messagingSenderId: "722694462465",
+  appId: "1:722694462465:web:954b08d127ca663713b400"
+};
+
+export default firebaseConfig;
