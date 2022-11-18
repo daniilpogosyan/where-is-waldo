ffa3cd4dfc1321815f61a051c120934a565aa66e
Daniil Pogosyan <daniilpogosyan@gmail.com>
2022-11-18 21:30:44 +0500

    Fix dropdown crossing viewport border
    If dropdown dropping to the right would cross the right side, it will
    drop to the left, instead of right side

--- src/pages/Game/components/DropDown.js
+++ src/pages/Game/components/DropDown.js
@@ -1,14 +1,36 @@
+import { useRef } from 'react';
 import './DropDown.css';
 
 export default function DropDown(props) {
-  if (props.position && props.targets.length > 0)
+  const dropdownRef = useRef(null);
+
+  if (props.position && props.targets.length > 0) {
+    // check if dropdownRef points to the dropdown and not to `null`
+    // it's required until the first render is occured
+    // default zero value are used in calculating actual position of the dropdown
+    let dropdownWidth = 0;
+    if (dropdownRef.current) {
+      dropdownWidth = dropdownRef.current.clientWidth;
+    }
+    
+
+    // position of the drop down with taking into account possible
+    // viewport overflowing
+    let positionWithoutCrossing = {};
+    const viewportRect = document.body.getBoundingClientRect();
+    if (props.position.x + dropdownWidth > viewportRect.width) {
+      positionWithoutCrossing.left = props.position.x - dropdownWidth + "px";
+    } else {
+      positionWithoutCrossing.left = props.position.x + "px";
+    }
+    
+    positionWithoutCrossing.top = props.position.y + "px";
+
     return (
       <ul
+        ref={dropdownRef}
         className="drop-down"
-        style={{
-          left: props.position.x + "px",
-          top: props.position.y + "px",
-        }}
+        style={positionWithoutCrossing}
       >
         {props.targets.map(target => (
           <li
@@ -20,6 +42,7 @@ export default function DropDown(props) {
         ))}
       </ul>
     )
+  }
 
   return null
 }
