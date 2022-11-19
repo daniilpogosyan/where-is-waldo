79a182bb0e8c9f2f5541557afad1fb02342181ef
Daniil Pogosyan <daniilpogosyan@gmail.com>
2022-11-19 15:48:01 +0500

    Style modal window and its content

--- src/pages/Game/components/GameModalWindow.js
+++ src/pages/Game/components/GameModalWindow.js
@@ -1,27 +1,34 @@
 import ModalWindow from '../../../components/ModalWindow';
 import Clock from '../../../components/Clock';
 
+import style from './GameModalWindow.module.css';
+
 export default function GameModalWindow(props) {
   return (
     <ModalWindow>
-      {
-        props.lastGameTime 
-        ? (
-          <p className="main-text">
-            You did it for <Clock millisec={props.lastGameTime} />.
-          </p>
-        )
-        : null
-      }
-      {
-        props.gameIsReady
-        ? (
-          <button onClick={props.startGame}>
-            {props.lastGameTime ? "Play Again" : "Start!"}
-          </button>
-        )
-        : <p className='sub-text'>Loading...</p>
-      }
+      <div className={`${style['content']}`}>
+        {
+          props.lastGameTime
+          ? (
+            <p className="main-text">
+              You did it for <Clock millisec={props.lastGameTime} />.
+            </p>
+          )
+          : null
+        }
+        {
+          props.gameIsReady
+          ? (
+            <button
+              onClick={props.startGame}
+              className={`${style['button']} main-text`}
+            >
+              {props.lastGameTime ? "Play Again" : "Start!"}
+            </button>
+          )
+          : <p className='sub-text'>Loading...</p>
+        }
+      </div>
     </ModalWindow>
   )
 }
